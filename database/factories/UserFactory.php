a0717450f9dd5a0670d9c1c5a4e52579e1184159
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-03-09 22:20:54 +0200

    defined factory to generate fake data for threads

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,13 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Thread::class, function ($faker) {
+    return [
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
