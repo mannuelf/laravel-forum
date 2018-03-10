4d3b92937697f5a07ba82df8cae3dd5fe9534f29
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-03-10 05:48:18 +0200

    setup first unit test and in memory sqlite in memory config in xml file

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -5,16 +5,16 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_browse_threads()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/threads');
+        $this->assertStatus(200);
     }
 }
